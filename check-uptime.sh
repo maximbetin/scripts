d64eaf81db1606c981a2e53730f9d65c46042515
maximbetin <betinmaxim@gmail.com>
2020-11-17 16:26:40 +0100

    Create check-uptime.sh

---
+++ check-uptime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Infinite loop that keeps sending curl HTTP requests to the endpoint every 1 second. If the returned HTTP code is not 200, displays a warning message.
+
+while :
+do
+
+	if [ $(curl -s -o /dev/null -w "%{http_code}" example.com) -ne 200 ]; then
+		echo "Site is down!"
+	fi
+	sleep 1
+  
+done
