17361252cdba5cd9518ca4800189682f6d861351
maximbetin <betinmaxim@gmail.com>
2020-11-18 12:23:13 +0100

    Update k8s-resources.sh

--- k8s-resources.sh
+++ k8s-resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Gets all K8S defined resources, outputs them into yaml files and moves them into a corresponding folder.
+# Gets all K8S defined resources, creates a folder for each and moves them into the corresponding folder.
 
 for n in $(kubectl get -o=name pvc, configmap, serviceaccount, secret, ingress, service, deployment, statefulset, hpa, job, cronjob)
 do
