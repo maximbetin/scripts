9195c412a9fcec2681ea8172413302cb08d9479b
Maxim <betinmaxim@gmail.com>
2021-02-02 21:05:39 +0100

    Update and rename k8s.sh to random.sh

--- random.sh
+++ random.sh
@@ -2,3 +2,5 @@
 kubectl get pod | grep Evicted | awk '{print $1}' | xargs kubectl delete pod
 # Delete errored out pods.
 kubectl get pods | grep Error | cut -d' ' -f 1 | xargs kubectl delete pod
+# Remove all stopped Docker containers.
+docker container rm $(docker container ls –aq)
