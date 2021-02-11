f728d0ae32b7d4325a3fa7f99c37c9b14393cdb9
Maxim <betinmaxim@gmail.com>
2021-02-11 19:09:49 +0100

    Update random.sh

--- random.sh
+++ random.sh
@@ -4,3 +4,10 @@ kubectl get pod | grep Evicted | awk '{print $1}' | xargs kubectl delete pod
 kubectl get pods | grep Error | cut -d' ' -f 1 | xargs kubectl delete pod
 # Remove all stopped Docker containers.
 docker container rm $(docker container ls –aq)
+# Drain node pools one by one
+for node in $(kubectl get node | grep SchedulingDisabled | cut -d " " -f1)
+do
+    echo kubectl drain $node
+    kubectl drain $node --force --delete-emptydir-data --ignore-daemonsets
+    sleep 600
+done
