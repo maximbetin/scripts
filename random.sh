1cfce5e4c8d132a12afb6a4b8bddd0d3a94357cf
Maxim <betinmaxim@gmail.com>
2022-03-27 17:24:12 +0200

    Update random.sh
    Delete all Docker images and containers one-liner

--- random.sh
+++ random.sh
@@ -1,2 +1,2 @@
-# Remove all stopped Docker containers.
-docker container rm $(docker container ls –aq)
+# Remove all Docker images, stop all Docker containers and remove all Docker containers one-liner
+docker rmi $(docker images -a -q) && docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
