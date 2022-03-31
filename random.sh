6030839a851fd3059248251222bdc8a4e2263148
Maxim <betinmaxim@gmail.com>
2022-03-31 16:28:05 +0200

    Update random.sh

--- random.sh
+++ random.sh
@@ -1,2 +1,8 @@
 # Remove all Docker images, stop all Docker containers and remove all Docker containers one-liner
 docker rmi $(docker images -a -q) && docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+
+# Delete all jobs with failed pods
+kubectl delete job --ignore-not-found=true $(kubectl get job -o=jsonpath='{.items[?(@.status.failed)].metadata.name}' -n <namespace>) -n <namespace>
+
+# Delete all pods in Error
+kubectl get pods -n <namespace> | grep Error | awk '{print $1}' | xargs kubectl delete pod -n <namespace>
