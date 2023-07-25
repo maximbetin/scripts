86990eca559eb3013dd312d8c827799358e5c983
Maxim <betinmaxim@gmail.com>
2023-07-25 09:58:46 +0200

    Update random.sh

--- random.sh
+++ random.sh
@@ -2,8 +2,14 @@
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -a -q)
 
 namespace="<namespace>"
+
 # Delete all jobs with failed pods
 kubectl delete job --ignore-not-found=true $(kubectl get job -o=jsonpath='{.items[?(@.status.failed)].metadata.name}' -n $namespace) -n $namespace 2>/dev/null || echo -e "No Failed jobs to clean"
 
 # Delete all pods in Error
 kubectl get pods -n $namespace | grep Error | awk '{print $1}' | xargs kubectl delete pod -n $namespace 2>/dev/null || echo -e "No Error pods to clean"
+
+# Clean up ReplicaSets
+kubectl get rs -n $namespace -o=jsonpath='{range .items[?(@.spec.replicas=="0" && @.status.replicas=="0" && @.status.readyReplicas=="0")]}{.metadata.name}{"\n"}' | xargs kubectl delete rs -n $namespace
+
+
