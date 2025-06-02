c20b29b22469a63a244b8429350c7f214834d467
Maxim Betin <betinmaxim@gmail.com>
2025-06-02 20:16:52 +0200

    feat: add Get-AllInstalledSoftware function to retrieve installed software details

---
+++ Get-AllInstalledSoftware.ps1
@@ -0,0 +1,62 @@
+Function Get-AllInstalledSoftware {
+  [CmdletBinding()]
+  Param()
+
+  $softwareList = New-Object System.Collections.Generic.List[PSObject]
+
+  # 1. Get-Package (for traditional and managed packages)
+  try {
+    Get-Package | ForEach-Object {
+      $softwareList.Add([PSCustomObject]@{
+          Name        = $_.Name
+          Version     = $_.Version
+          Publisher   = $_.ProviderName # Using ProviderName as a stand-in for Publisher here
+          InstallDate = $null
+          Source      = "Get-Package"
+        })
+    }
+  } catch {
+    Write-Warning "Could not retrieve packages using Get-Package: $($_.Exception.Message)"
+  }
+
+  # 2. Registry (32-bit and 64-bit applications)
+  $registryPaths = @(
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
+  )
+
+  foreach ($path in $registryPaths) {
+    try {
+      Get-ItemProperty $path | Where-Object { $null -ne $_.DisplayName } | ForEach-Object {
+        $softwareList.Add([PSCustomObject]@{
+            Name        = $_.DisplayName
+            Version     = $_.DisplayVersion
+            Publisher   = $_.Publisher
+            InstallDate = $_.InstallDate
+            Source      = "Registry ($path)"
+          })
+      }
+    } catch {
+      Write-Warning "Could not retrieve software from registry path '$path': $($_.Exception.Message)"
+    }
+  }
+
+  # 3. UWP/Microsoft Store Apps
+  try {
+    Get-AppxPackage | ForEach-Object {
+      $softwareList.Add([PSCustomObject]@{
+          Name        = $_.Name
+          Version     = $_.Version
+          Publisher   = $_.Publisher # UWP apps often have a Publisher property
+          InstallDate = $null
+          Source      = "UWP/AppX"
+        })
+    }
+  } catch {
+    Write-Warning "Could not retrieve UWP/AppX packages: $($_.Exception.Message)"
+  }
+
+  # Filter out duplicates and format
+  $softwareList | Sort-Object Name | Select-Object -Unique Name, Version, Publisher, InstallDate, Source | Format-Table -AutoSize
+}
